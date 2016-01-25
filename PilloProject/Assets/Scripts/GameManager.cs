df68b137501f8ebd6d917eafede4ab381fd81e54
Jordy <jordywollaars@kpnmail.nl>
2016-01-25 13:56:02 +0100

    Scripts
    Gamemanager heeft Functie voor aanvragen pilloInput

---
+++ PilloProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pillo;
+
+public class GameManager : MonoBehaviour
+{
+    private PilloController m_PC;
+
+    void Start()
+    {
+        PilloController.ConfigureSensorRange(0x50, 0x6f);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public float GetPilloSensorByID(PilloID pilloID)
+    {
+        return PilloController.GetSensor(pilloID);
+    }
+}
