04042cb8d31f2b5fc686c5f428f549dbba4e0aae
Jordy <jordywollaars@kpnmail.nl>
2016-01-25 14:37:37 +0100

    GameManager getfuncties + CarController Accaleration

--- PilloProject/Assets/Scripts/CarController.cs
+++ PilloProject/Assets/Scripts/CarController.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Pillo;
 
-public class CarController : MonoBehaviour {
+public class CarController : MonoBehaviour
+{
+    private float m_PilloInput;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField]
+    private int m_PlayerID;
+
+    private float m_Acceleration = 2f;
+    private float m_DeAcceleration = 1f;
+
+    private float m_MinMPH = 0f;
+    public float m_CurrentMPH = 0f;
+    private float m_MaxMPH = 120f;
+
+    private GameManager m_GameManager;
+
+    private void Start()
+    {
+        m_GameManager = GameObject.Find("Scripts").GetComponent<GameManager>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        m_PilloInput = m_GameManager.GetPilloSensorByID((PilloID)m_PlayerID);
+
+        if (m_PilloInput > 0.01f)
+        {
+            if (m_CurrentMPH < m_MaxMPH)
+            {
+                m_CurrentMPH += m_Acceleration;
+            }
+            else
+            {
+                m_CurrentMPH = m_MaxMPH;
+            }
+        }
+        else if (m_PilloInput < 0.01f)
+        {
+            if (m_CurrentMPH > m_MinMPH)
+            {
+                m_CurrentMPH -= m_DeAcceleration;
+            }
+        }
+    }
 }
--- PilloProject/Assets/Scripts/GameManager.cs
+++ PilloProject/Assets/Scripts/GameManager.cs
@@ -6,12 +6,19 @@ public class GameManager : MonoBehaviour
 {
     private PilloController m_PC;
 
+    [SerializeField][Range(0, 2f)]
+    private int[] m_TeamSL = new int[2];
+    private int[] m_TeamCL;
+
     void Start()
     {
         PilloController.ConfigureSensorRange(0x50, 0x6f);
+
+        //m_TeamCL[0] = m_TeamSL[0];
+        //m_TeamCL[1] = m_TeamSL[1];
     }
 
-    void FixedUpdate()
+    void Update()
     {
 
     }
@@ -19,5 +26,10 @@ public class GameManager : MonoBehaviour
     public float GetPilloSensorByID(PilloID pilloID)
     {
         return PilloController.GetSensor(pilloID);
+    }
+
+    public int GetCurrentLaneByID(int ID)
+    {
+        return m_TeamCL[ID];
     }
 }
