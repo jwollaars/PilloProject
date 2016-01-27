4f4b05f9aa40e0abef9ad62bd3514876330acf60
Jordy <jordywollaars@kpnmail.nl>
2016-01-27 08:53:52 +0100

    TADA

--- PilloProject/Assets/Scripts/CarController.cs
+++ PilloProject/Assets/Scripts/CarController.cs
@@ -5,12 +5,11 @@ using Pillo;
 public class CarController : MonoBehaviour
 {
     [SerializeField]
-    private int m_PlayerIDAccelerate;
-    [SerializeField]
-    private int m_PlayerIDSteer;
+    private int m_PlayerID;
 
-    private float m_PilloInputAccelerate;
-    private float m_PilloInputSteer;
+    private float m_InputHorizontal;
+    private float m_InputTriggerLeft;
+    private float m_InputTriggerRight;
 
     private float m_Acceleration = 10f;
     private float m_CurrentAcceleration = 10f;
@@ -22,35 +21,29 @@ public class CarController : MonoBehaviour
     public float m_TargetMPH = 0f;
     private float m_MaxMPH = 120f;
 
-    private float m_SteerSpeed = 20f;
-    private float m_MinRotation = -360f;
-    private float m_CurrentRotation = 360f;
-    private float m_MaxRotation = 360f;
-
-    private GameManager m_GameManager;
-
     private void Start()
     {
-        m_GameManager = GameObject.Find("Scripts").GetComponent<GameManager>();
+
     }
 
     private void FixedUpdate()
     {
-        Accelerate();
-        Steer();
+        Movement();
     }
 
-    private void Accelerate()
+    private void Movement()
     {
-        m_PilloInputAccelerate = m_GameManager.GetPilloSensorByID((PilloID)m_PlayerIDAccelerate);
+        m_InputHorizontal = Input.GetAxisRaw("Horizontal " + m_PlayerID);
+        m_InputTriggerLeft = Input.GetAxisRaw("Left Trigger " + m_PlayerID);
+        m_InputTriggerRight = Input.GetAxisRaw("Right Trigger " + m_PlayerID);
 
-        m_TargetMPH = m_MaxMPH * m_PilloInputAccelerate;
+        m_TargetMPH = m_MaxMPH * m_InputTriggerRight;
 
         if (m_CurrentMPH < m_TargetMPH)
         {
             m_CurrentDeAcceleration = m_DeAcceleration;
             m_CurrentMPH += m_CurrentAcceleration;
-            m_CurrentAcceleration = (m_Acceleration / m_CurrentMPH) * m_PilloInputAccelerate;
+            m_CurrentAcceleration = (m_Acceleration / m_CurrentMPH) * m_InputTriggerRight;
         }
         else if (m_CurrentMPH > m_TargetMPH)
         {
@@ -59,35 +52,17 @@ public class CarController : MonoBehaviour
             m_CurrentDeAcceleration = m_DeAcceleration / m_CurrentMPH;
         }
 
+        if (m_InputTriggerLeft > 0.75f)
+        {
+            m_DeAcceleration *= 30f;
+        }
+        else
+        {
+            m_DeAcceleration /= 15f;
+        }
+
         m_CurrentMPH = Mathf.Clamp(m_CurrentMPH, m_MinMPH, m_MaxMPH);
 
         transform.Translate((-transform.forward * m_CurrentMPH) * 0.5f * Time.deltaTime);
-    }
-
-    private void Steer()
-    {
-        m_PilloInputSteer = m_GameManager.GetPilloSensorByID((PilloID)m_PlayerIDSteer);
-
-        //if (m_PilloInputSteer >= 0f && m_PilloInputSteer < 0.4f)
-        //{
-        //    if (m_CurrentRotation < -90f)
-        //    {
-        //        m_CurrentRotation -= m_SteerSpeed * Time.deltaTime;
-        //    }
-        //}
-        //else if (m_PilloInputSteer > 0.6f && m_PilloInputSteer <= 1f)
-        //{
-        //    if (m_CurrentRotation < 90f)
-        //    {
-        //        m_CurrentRotation += m_SteerSpeed * Time.deltaTime;
-        //    }
-        //}
-
-        
-
-        m_CurrentRotation = 360f * m_PilloInputSteer;
-        m_CurrentRotation = Mathf.Clamp(m_CurrentRotation, 0f, 360f);
-
-        transform.rotation = Quaternion.Euler(new Vector3(0f, m_CurrentRotation, 0f));
     }
 }
