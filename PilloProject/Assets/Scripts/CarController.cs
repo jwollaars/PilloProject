893702e3a376bf9bf5e14ceed6a71e811561c548
510691 <510691@student.glu.nl>
2016-01-27 08:58:57 +0100

    Car Controller Keyboard

--- PilloProject/Assets/Scripts/CarController.cs
+++ PilloProject/Assets/Scripts/CarController.cs
@@ -1,68 +1,150 @@
 ﻿using UnityEngine;
 using System.Collections;
-using Pillo;
+using System.Collections.Generic;
 
 public class CarController : MonoBehaviour
 {
-    [SerializeField]
-    private int m_PlayerID;
-
     private float m_InputHorizontal;
-    private float m_InputTriggerLeft;
-    private float m_InputTriggerRight;
+    private float m_InputVertical;
 
-    private float m_Acceleration = 10f;
-    private float m_CurrentAcceleration = 10f;
-    private float m_DeAcceleration = 15f;
-    private float m_CurrentDeAcceleration = 15f;
+    private bool m_BlockInput;
+
+    private float m_Acceleration = 20f;
+    private float m_CurrentAcceleration = 20f;
+    private float m_DeAcceleration = 40f;
+    private float m_CurrentDeAcceleration = 40f;
 
     private float m_MinMPH = 0f;
-    public float m_CurrentMPH = 0f;
-    public float m_TargetMPH = 0f;
+    private float m_CurrentMPH = 0f;
     private float m_MaxMPH = 120f;
+    private float m_TargetMPH = 0f;
+
+    private bool m_Respawn = false;
+    private float m_CurrentRespawnTimer = 3f;
+    private float m_RespawnTimer = 3f;
+
+    [SerializeField]
+    private GameObject m_StartRespawnPoint;
+    private Stack<GameObject> m_RespawnPoints;
 
     private void Start()
     {
-
+        m_RespawnPoints = new Stack<GameObject>();
+        m_RespawnPoints.Push(m_StartRespawnPoint);
     }
 
     private void FixedUpdate()
     {
-        Movement();
+        GasAndSteer();
+        Respawn();
     }
 
-    private void Movement()
+    private void OnTriggerEnter(Collider collider)
     {
-        m_InputHorizontal = Input.GetAxisRaw("Horizontal " + m_PlayerID);
-        m_InputTriggerLeft = Input.GetAxisRaw("Left Trigger " + m_PlayerID);
-        m_InputTriggerRight = Input.GetAxisRaw("Right Trigger " + m_PlayerID);
-
-        m_TargetMPH = m_MaxMPH * m_InputTriggerRight;
-
-        if (m_CurrentMPH < m_TargetMPH)
+        if (collider.gameObject.tag == "Respawn Point")
         {
-            m_CurrentDeAcceleration = m_DeAcceleration;
-            m_CurrentMPH += m_CurrentAcceleration;
-            m_CurrentAcceleration = (m_Acceleration / m_CurrentMPH) * m_InputTriggerRight;
+            m_RespawnPoints.Push(collider.gameObject);
+            Debug.Log(m_RespawnPoints.Peek().name);
         }
-        else if (m_CurrentMPH > m_TargetMPH)
-        {
-            m_CurrentAcceleration = m_Acceleration;
-            m_CurrentMPH -= m_CurrentDeAcceleration;
-            m_CurrentDeAcceleration = m_DeAcceleration / m_CurrentMPH;
-        }
+    }
 
-        if (m_InputTriggerLeft > 0.75f)
+    private void GasAndSteer()
+    {
+        if (!m_BlockInput)
         {
-            m_DeAcceleration *= 30f;
+            m_InputHorizontal = Input.GetAxisRaw("Horizontal");
+            m_InputVertical = Input.GetAxisRaw("Vertical");
+
+
+            m_TargetMPH = m_MaxMPH * m_InputVertical;
+
+            if (m_InputVertical != 0f)
+            {
+                if (m_InputVertical >= 0.01f)
+                {
+                    if (m_CurrentMPH < m_TargetMPH)
+                    {
+                        m_CurrentMPH += m_CurrentAcceleration;
+                        m_CurrentAcceleration = (m_Acceleration / m_CurrentMPH) * m_InputVertical;
+                        m_CurrentDeAcceleration = m_DeAcceleration;
+                    }
+                }
+                else if (m_InputVertical <= -0.01f)
+                {
+                    //if (m_CurrentMPH > m_TargetMPH)
+                    //{
+                    //    m_CurrentMPH -= m_CurrentAcceleration;
+                    //    m_CurrentAcceleration = (m_Acceleration / m_CurrentMPH) * m_InputVertical;
+                    //    m_CurrentDeAcceleration = m_DeAcceleration;
+                    //}
+                }
+            }
+            else if (m_InputVertical == 0f)
+            {
+                if (m_CurrentMPH != m_TargetMPH)
+                {
+                    m_CurrentMPH -= m_CurrentDeAcceleration;
+                    m_CurrentDeAcceleration = m_DeAcceleration / m_CurrentMPH;
+                    m_CurrentAcceleration = m_Acceleration;
+                }
+            }
+
+            if (m_InputHorizontal != 0f)
+            {
+                transform.Rotate(new Vector3(0f, m_InputHorizontal * 100f * Time.deltaTime, 0f));
+            }
+
+            if (m_CurrentMPH > 0.01f)
+            {
+                if (m_CurrentMPH <= m_MinMPH)
+                {
+                    m_CurrentMPH = m_MinMPH;
+                }
+                else if (m_CurrentMPH >= m_MaxMPH)
+                {
+                    m_CurrentMPH = m_MaxMPH;
+                }
+            }
+            else if (m_CurrentMPH < -0.01f)
+            {
+                if (m_CurrentMPH <= -m_MinMPH)
+                {
+                    m_CurrentMPH = -m_MinMPH;
+                }
+                else if (m_CurrentMPH >= -m_MaxMPH)
+                {
+                    m_CurrentMPH = -m_MaxMPH;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                m_Respawn = true;
+            }
+
+            transform.Translate(Vector3.forward * m_CurrentMPH * Time.deltaTime);
         }
-        else
+    }
+
+    private void Respawn()
+    {
+        if (m_Respawn)
         {
-            m_DeAcceleration /= 15f;
+            m_BlockInput = true;
+
+            if (m_CurrentRespawnTimer > 0f)
+            {
+                m_CurrentRespawnTimer -= Time.deltaTime;
+            }
+
+            if (m_CurrentRespawnTimer <= 0f)
+            {
+                m_BlockInput = false;
+                transform.position = m_RespawnPoints.Peek().transform.position;
+                transform.rotation = m_RespawnPoints.Peek().transform.rotation;
+                m_Respawn = false;
+                m_CurrentRespawnTimer = m_RespawnTimer;
+            }
         }
-
-        m_CurrentMPH = Mathf.Clamp(m_CurrentMPH, m_MinMPH, m_MaxMPH);
-
-        transform.Translate((-transform.forward * m_CurrentMPH) * 0.5f * Time.deltaTime);
     }
 }
